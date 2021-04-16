d036ba9f349b0e7a94641a79c75e95fd256cf9b3
Myrla <75264586+myrlaf25@users.noreply.github.com>
2021-04-16 13:08:27 -0700

    update

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+// // tailwind.config.js
+// module.exports = {
+//     theme: {
+//       screens: {
+//         'tablet': '640px',
+//         => @media (min-width: 640px) { .text-center }
+  
+//         'laptop': '1024px',
+//         => @media (min-width: 1024px) { ... }
+  
+//         'desktop': '1280px',
+//         => @media (min-width: 1280px) { ... }
+//       },
+//     }
+//   }
